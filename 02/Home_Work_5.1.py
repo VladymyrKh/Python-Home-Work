24ea6f8f5e28f97a977e200e828810d426dae69d
VladymyrKh <124580934+VladymyrKh@users.noreply.github.com>
2026-02-06 14:43:47 +0200

    first commit

---
+++ 02/Home_Work_5.1.py
@@ -0,0 +1,16 @@
+import string
+import keyword
+
+word = input("Введіть дані")
+first_check = word[0].isdigit() if word else False
+second_check = any(i.isupper() for i in word)
+rep = string.punctuation.replace('_', '') + ' '
+third_check = any(i in rep for i in word)
+fourth_check = word.count('_') > 1
+fifth_check = word in keyword.kwlist
+sixth_check = len(word) == 0
+if (first_check or second_check or third_check or
+        fourth_check or fifth_check or sixth_check):
+    print(False)
+else:
+    print(True)
