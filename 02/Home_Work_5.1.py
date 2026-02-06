207f3375126d5facbb5cf853cadddf83efd5b13d
VladymyrKh <124580934+VladymyrKh@users.noreply.github.com>
2026-02-06 15:45:47 +0200

    second commit

--- 02/Home_Work_5.1.py
+++ 02/Home_Work_5.1.py
@@ -6,7 +6,7 @@ first_check = word[0].isdigit() if word else False
 second_check = any(i.isupper() for i in word)
 rep = string.punctuation.replace('_', '') + ' '
 third_check = any(i in rep for i in word)
-fourth_check = word.count('_') > 1
+fourth_check = '__' in word
 fifth_check = word in keyword.kwlist
 sixth_check = len(word) == 0
 if (first_check or second_check or third_check or
